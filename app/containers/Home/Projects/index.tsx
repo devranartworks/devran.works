c31d07c8d6c4f5717d20c6ef7c423bd78671ab72
Yılmaz Ev <yilmaz.ev@outlook.com>
2024-05-12 22:54:06 +0300

    bug-fix: projects grid issue fixed

--- app/containers/Home/Projects/index.tsx
+++ app/containers/Home/Projects/index.tsx
@@ -7,7 +7,7 @@ const Projects: React.FC = () => {
     return (
         <section id="projects" className="flex w-full justify-center px-3 py-8 lg:px-6 lg:py-16">
             <div className="w-full max-w-[1224px]">
-                <div className="grid-[repeat(auto-fit,minmax(90%,1fr))] md:grid-[repeat(auto-fit,minmax(392px,1fr))] grid gap-6">
+                <div className="grid grid-cols-[repeat(auto-fit,minmax(90%,1fr))] gap-6 sm:grid-cols-3">
                     {projectsData.projects.map((project, index) => (
                         <Link key={index} href={project.link} target="_blank" className="group max-w-[392px]" aria-label={project.title}>
                             <div className="relative overflow-hidden rounded-lg">
@@ -21,25 +21,6 @@ const Projects: React.FC = () => {
                         </Link>
                     ))}
                 </div>
-            </div>
-            <div>
-                {/* <div className="flex justify-center">
-                    <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-                        {projectsData.projects.map((project, index) => (
-                            <Link key={index} href={project.link} target="_blank" className="flex h-auto w-full max-w-sm flex-col rounded-xl border border-gray-100 bg-white p-6" aria-label={project.title}>
-                                <span className="mb-2 font-medium text-stone-700">{project.title}</span>
-                                <p className="leading-[1.6rem] text-stone-500">{project.description}</p>
-                                <div className="mt-3 flex flex-wrap items-center gap-1.5">
-                                    {project.used.map((skill, skillIndex) => (
-                                        <div key={skillIndex} className="rounded-full border border-gray-100 bg-white px-3 py-1.5">
-                                            <p className="text-sm text-stone-500">{skill.name}</p>
-                                        </div>
-                                    ))}
-                                </div>
-                            </Link>
-                        ))}
-                    </div>
-                </div> */}
             </div>
         </section>
     )
