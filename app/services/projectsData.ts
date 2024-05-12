fdec39c722d588b6c427d323d909348712396c80
devranduzgun <devranduzgunn@gmail.com>
2024-05-12 22:23:51 +0300

    Update projectsData.ts

--- app/services/projectsData.ts
+++ app/services/projectsData.ts
@@ -1,12 +1,18 @@
 import { ProjectsProps } from "../types/projectTypes"
-import ExampleProject from "../assets/images/example-project.png"
+import helvaci from "../assets/images/helvaci.png"
+import finance from "../assets/images/finance.png"
 
 const projectsData: { projects: ProjectsProps[] } = {
     projects: [
         {
-            link: "https://domain.com",
-            image: ExampleProject,
-            title: "Example project",
+            link: "https://www.behance.net/gallery/198412143/Helvac-Hamdi-UI-UX-design?",
+            image: helvaci,
+            title: "Helvacı - UI & UX",
+        },
+        {
+            link: "https://www.behance.net/gallery/198411561/Finance-App-UI-UX?",
+            image: finance,
+            title: "Finance App - UI & UX",
         },
     ],
 }
