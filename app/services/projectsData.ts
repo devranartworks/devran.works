1cd1c214a6d53ac6e6e7831f2d4366b6b84a8960
devran <devranduzgunn@gmail.com>
2024-05-16 08:30:37 +0300

    Update projectsData.ts

--- app/services/projectsData.ts
+++ app/services/projectsData.ts
@@ -1,7 +1,8 @@
 import { ProjectsProps } from "../types/projectTypes"
 import helvaci from "../assets/images/helvaci.png"
 import finance from "../assets/images/finance.png"
-import void from "../assets/images/voidap.png"
+import void from "../assets/images/voidap.jpg"
+import blockchain1 from "../assets/images/425318079_323111736816946_6054574089857207142_n.jpg"
 
 const projectsData: { projects: ProjectsProps[] } = {
     projects: [
@@ -19,6 +20,11 @@ const projectsData: { projects: ProjectsProps[] } = {
             link: "https://www.behance.net/gallery/198710159/Void-App-UI-UX",
             image: void,
             title: "Void App - UI & UX",
+        },
+        {
+            link: "https://www.behance.net/gallery/198710553/Anonim-Blockchain-Project-UI-UX?",
+            image: blockchain1,
+            title: "Blockchain Project - UI & UX",
         },
     ],
 }
