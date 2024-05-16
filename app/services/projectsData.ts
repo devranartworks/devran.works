754678322d8fc409924f6533d64ebc044674ea01
devran <devranduzgunn@gmail.com>
2024-05-16 14:08:36 +0300

    Update projectsData.ts

--- app/services/projectsData.ts
+++ app/services/projectsData.ts
@@ -1,24 +1,24 @@
 import { ProjectsProps } from "../types/projectTypes"
 import helvaci from "../assets/images/helvaci.png"
 import finance from "../assets/images/finance.png"
-import void from "../assets/images/voidap.jpg"
+import voidap from "../assets/images/voidap.jpg"
 import blockchain1 from "../assets/images/425318079_323111736816946_6054574089857207142_n.jpg"
 
 const projectsData: { projects: ProjectsProps[] } = {
     projects: [
-        {
-            link: "https://www.behance.net/gallery/198412143/Helvac-Hamdi-UI-UX-design?",
-            image: helvaci,
-            title: "Helvacı - UI & UX",
-        },
         {
             link: "https://www.behance.net/gallery/198411561/Finance-App-UI-UX?",
             image: finance,
             title: "Finance App - UI & UX",
         },
         {
+            link: "https://www.behance.net/gallery/198412143/Helvac-Hamdi-UI-UX-design?",
+            image: helvaci,
+            title: "Helvacı - UI & UX",
+        },
+        {
             link: "https://www.behance.net/gallery/198710159/Void-App-UI-UX",
-            image: void,
+            image: voidap,
             title: "Void App - UI & UX",
         },
         {
